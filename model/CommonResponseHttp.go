a88f3ac6495e8dae441c752176463f289dcaca19
BachVanHai <haibv@bmktech.com.vn>
2026-02-02 17:38:18 +0700

    no message

--- model/CommonResponseHttp.go
+++ model/CommonResponseHttp.go
@@ -1,9 +1,16 @@
 package model
 
+import "fmt"
+
 type CommonResponseHttp struct {
 	Status          int    `json:"status"`
 	Message         string `json:"message"`
 	Code            string `json:"code,omitempty"` // Mã lỗi riêng của hệ thống
 	Data            any    `json:"data,omitempty"`
 	ClientMessageId string `json:"clientMessageId,omitempty"`
+}
+
+// Hàm này giúp struct này thỏa mãn interface 'error' của Go
+func (e *CommonResponseHttp) Error() string {
+	return fmt.Sprintf("[%s] %s", e.Code, e.Message)
 }
