0233f329ac92a90d3f0f46294d68b1d4bc9e5e1d
BachVanHai <haibv@bmktech.com.vn>
2026-02-02 15:18:37 +0700

    init common

---
+++ exception/CommonErrHandler.go
@@ -0,0 +1,38 @@
+package exception
+
+import (
+	"errors"
+	"fmt"
+	"my-go-common-lib/model"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func CommonErrHandler(err error, c echo.Context) {
+	code := http.StatusInternalServerError
+	message := "Internal Server Error"
+
+	// Kiểm tra nếu là lỗi của Echo (ví dụ 404, 405)
+	var he *echo.HTTPError
+	if errors.As(err, &he) {
+		code = he.Code
+		message = fmt.Sprintf("%v", he.Message)
+	}
+
+	// Bạn có thể thêm các check lỗi logic nghiệp vụ tại đây
+	// if myErr, ok := err.(*MyBusinessError); ok { ... }
+
+	// Gửi response theo format chuẩn
+	errorResponse := model.CommonResponseHttp{
+		Status:  code,
+		Message: message,
+	}
+
+	if !c.Response().Committed {
+		err := c.JSON(code, errorResponse)
+		if err != nil {
+			return
+		}
+	}
+}
---
+++ model/CommonResponseHttp.go
@@ -0,0 +1,8 @@
+package model
+
+type CommonResponseHttp struct {
+	Status  int    `json:"status"`
+	Message string `json:"message"`
+	Code    string `json:"code,omitempty"` // Mã lỗi riêng của hệ thống
+	Data    any    `json:"data,omitempty"`
+}
